eb436c583c1762c4f2ab240bd70bb060f97cff4a
Buse Keklik <76210283+busekeklik@users.noreply.github.com>
2023-01-01 10:51:34 +0300

    Add files via upload

---
+++ Assets/Scripts/piecesScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class piecesScript : MonoBehaviour
+{
+    private Vector3 RightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+
+    void Start()
+    {
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(5,11),Random.Range(9, -4));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, RightPosition) < 0.5f)
+        {
+            if(!Selected)
+            {
+                if(InRightPosition == false)
+                {
+                    transform.position = RightPosition;
+                    InRightPosition= true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+
+                }
+            }
+        }
+    }
+}
